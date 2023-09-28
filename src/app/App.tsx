61ccc15b7be1b9ecfd3ee0feb28b5e0bd38defd5
ratiomark <yanagae@gmail.com>
2023-09-28 19:00:52 +0300

    add: toast rtk

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,20 @@
-import { Suspense, useEffect, useLayoutEffect } from 'react'
+import { Suspense, useEffect } from 'react'
 import { AppRouter } from './providers/router/AppRouter'
 import { LoaderWidget } from '@/widgets/LoaderWidget'
 import { useAppDispatch } from '@/shared/lib/helpers/hooks/useAppDispatch'
 import { useSelector } from 'react-redux'
-import { getUserAuthData, getUserMounted, initAuthData } from '@/entities/User'
-// import * as Toast from '@radix-ui/react-toast';
-import { useTheme } from '@/shared/context/useTheme'
+import { getUserMounted, initAuthData } from '@/entities/User'
 import { Header, HeaderSkeleton } from '@/widgets/Header'
 import './styles/regularStyles.css'
-import clsx from 'clsx'
-import { MyToastsRTK } from '@/shared/ui/Toast'
 
 
 export const App = () => {
 	const userMounted = useSelector(getUserMounted)
 	const dispatch = useAppDispatch()
-	// const { theme } = useTheme()
-
-
-	// useEffect(() => {
-	// document.body.className = theme
-	// }, [theme])
 
 	useEffect(() => {
 		dispatch(initAuthData())
 	}, [dispatch, userMounted])
-
-
 
 	if (!userMounted) return <LoaderWidget />
 
@@ -37,9 +25,6 @@ export const App = () => {
 				<Header />
 			</Suspense>
 			<AppRouter />
-			{/* <Suspense fallback={null}>
-				<MyToastsRTK />
-			</Suspense> */}
 			{/* <img src="https://i.pinimg.com/originals/e5/e8/30/e5e830f89f89f0259e1d705e14a5de93.gif" alt="Your GIF" id="my-gif" /> */}
 			{/* <Toast.Viewport className='viewport' /> */}
 		</div>
