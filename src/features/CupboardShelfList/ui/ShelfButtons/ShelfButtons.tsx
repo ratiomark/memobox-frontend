fd5eaeb1212cb136c65ce388d1ea7474c1aa18cb
ratiomark <yanagae@gmail.com>
2023-10-02 00:47:11 +0300

    ref: remove unused code ShelfButtons

--- src/features/CupboardShelfList/ui/ShelfButtons/ShelfButtons.tsx
+++ src/features/CupboardShelfList/ui/ShelfButtons/ShelfButtons.tsx
@@ -1,23 +1,18 @@
+import clsx from 'clsx';
 import ArrowBottomIcon from '@/shared/assets/icons/arrow-bottom.svg';
 import { Button } from '@/shared/ui/Button';
 import { Icon } from '@/shared/ui/Icon';
-import clsx from 'clsx';
 import { useTranslation } from 'react-i18next';
-import cls from './ShelfButtons.module.scss';
-import { memo, useCallback, useEffect, useRef, useState } from 'react';
+import { memo, useCallback} from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useNavigate } from 'react-router-dom';
 import { obtainRouteTraining, obtainRouteView } from '@/app/providers/router/config/routeConfig/routeConfig';
-import { AppLink } from '@/shared/ui/AppLink/AppLink';
-import { Dropdown } from '@/shared/ui/Popup';
-import { DropdownItem } from '@/shared/ui/Popup/ui/Dropdown/Dropdown';
 import { SettingButton } from '../SettingsButton/SettingsButton';
-import { ShelfSchema, useUpdateShelfMutation, useUpdateShelfWithTagMutation } from '@/entities/Shelf';
+import { ShelfSchema, useUpdateShelfMutation } from '@/entities/Shelf';
 import { DURATION_SHELF_COLLAPSING_SEC } from '@/shared/const/animation';
-import { useDebounce } from '@/shared/lib/helpers/hooks/useDebounce';
 import { useThrottle } from '@/shared/lib/helpers/hooks/useThrottle';
 import { dataAttrButtonTypeAddCard, dataAttrButtonTypeTrain } from '@/shared/const/idsAndDataAttributes';
-
+import cls from './ShelfButtons.module.scss';
 
 interface ShelfButtonsProps {
 	className?: string
@@ -55,24 +50,6 @@ export const ShelfButtons = memo((props: ShelfButtonsProps) => {
 		navigate(obtainRouteTraining(shelfId, 'all'))
 	}
 
-	useEffect(() => {
-		// fetch('http://localhost:8000/data/cupboard/shelves', {
-		// 	headers: { Authorization: 'token' },
-		// 	method: 'GET',
-		// }).then(response => {
-		// 	if (!response.ok) {
-		// 		throw new Error(`Ошибка, статус ${response.status}`);
-		// 	}
-		// 	return response.json();
-		// })
-		// 	.then(data => {
-		// 		console.log(data);
-		// 	})
-		// 	.catch(error => {
-		// 		console.error(error);
-		// 	});
-	}, [])
-
 	const onAddNewCardHandle = useCallback(() => {
 		onAddNewCardClick(shelfId)
 	}, [onAddNewCardClick, shelfId])
@@ -87,7 +64,6 @@ export const ShelfButtons = memo((props: ShelfButtonsProps) => {
 
 
 	const onCollapseClickHandle = useCallback(() => {
-		console.log('!!!!!!!!!!!!!!!!!!!!!!!!!click')
 		onCollapseClick(shelfId, !isCollapsed)
 		updateShelfMutation({ ...props.shelf, isCollapsed: !isCollapsed })
 	}, [onCollapseClick, shelfId, isCollapsed, updateShelfMutation, props.shelf])
