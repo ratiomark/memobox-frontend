6f2bcdfdd623da224a4cd3f2101f97eb7f18d4b1
ratiomark <yanagae@gmail.com>
2023-10-04 17:59:09 +0300

    remove unused code and files

--- src/widgets/ShelvesListViewWidget/index.ts
+++ src/widgets/ShelvesListViewWidget/index.ts
@@ -1,3 +1,1 @@
-export { shelvesListViewWidgetReducer, shelvesListViewWidgetActions } from './model/slice/shelvesListViewWidgetSlice';
-export type { ShelvesListViewWidgetSchema } from './model/types/ShelvesListViewWidgetSchema';
 export { ShelvesListViewWidget } from './ui/ShelvesListViewWidget';
--- src/widgets/ShelvesListViewWidget/model/selectors/getShelvesListViewWidget.ts
+++
@@ -1,3 +0,0 @@
-import { StateSchema } from '@/app/providers/StoreProvider';
-
-// export const getShelvesListViewWidget = (state: StateSchema) => state.shelvesListViewWidget
