b813f9de59c882abd10b29a9422a185d158279f5
ratiomark <yanagae@gmail.com>
2023-12-04 04:37:58 +0200

    add: udpate missedTraining and notifications

--- src/entities/User/model/services/userSettings/updateMissedTrainingThunk.ts
+++ src/entities/User/model/services/userSettings/updateMissedTrainingThunk.ts
@@ -1,35 +1,55 @@
 import { StateSchema, ThunkExtraArg } from '@/app/providers/StoreProvider'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { rtkApiUpdateMissedTraining } from '../../api/userApi'
-import { getJsonSettings } from '../../selectors/getJsonSettings'
-import { getUserAuthData } from '../../selectors/getUserAuthData'
 import { MissedTrainingValue } from '../../types/userSettings'
+import { toastsActions } from '@/shared/ui/Toast'
+import { t } from 'i18next'
+import { userActions } from '../../slice/userSlice'
 
-export const updateMissedTrainingThunk = () => { }
-// createAsyncThunk третьим аргументом принимает конфиг и там я могу описать поле extra и теперь обращаясь в thunkAPI.extra ТС подхватит то, что я описал в ThunkExtraArg
-// export const updateMissedTrainingThunk = createAsyncThunk<MissedTrainingValue, MissedTrainingValue, { rejectValue: string, extra: ThunkExtraArg, state: StateSchema }>(
-// 	'user/updateMissedTrainingThunk',
-// 	async (missedTraining, thunkAPI) => {
+const toastId = 'update-missedTraining'
+export const updateMissedTrainingThunk = createAsyncThunk<MissedTrainingValue, { missedTraining: MissedTrainingValue }, { rejectValue: string, extra: ThunkExtraArg, state: StateSchema }>(
+	'user/updateMissedTrainingThunk',
+	async ({ missedTraining }, thunkAPI) => {
 
-// 		const { dispatch, getState, } = thunkAPI
-// 		const userData = getUserAuthData(getState())
-// 		const currentUserSettings = getJsonSettings(getState())
+		const { dispatch, getState, } = thunkAPI
+		dispatch(userActions.setSettingsAwaitingResponse({ settings: 'missedTraining', isLoading: true }))
+		dispatch(toastsActions.addToast({
+			id: toastId,
+			toast: {
+				status: 'pending',
+				messageLoading: t('toast:messageLoading'),
+				messageError: t('toast:messageError'),
+				messageSuccess: t('toast:update_settings.messageSuccess'),
+			}
+		}))
 
-// 		if (!userData) return thunkAPI.rejectWithValue('Нет userData')
 
-// 		try {
-// 			const response = await dispatch(rtkApiUpdateMissedTraining({ missedTraining })).unwrap() //разворачиваю в реальный результат
 
-// 			// const jsonSettingsFromResponse = response.jsonSettings
+		try {
+			const response = await dispatch(rtkApiUpdateMissedTraining({ missedTraining })).unwrap() //
+			dispatch(userActions.setSettingsAwaitingResponse({
+				settings: 'missedTraining',
+				isLoading: false
+			}))
+			dispatch(toastsActions.updateToastById({
+				id: toastId,
+				toast: { status: 'success' }
+			}))
+			console.log(response)
+			return response.missedTraining
 
-// 			// if (!jsonSettingsFromResponse) return thunkAPI.rejectWithValue('Нет userData')
-
-// 			return response.missedTraining
-
-// 		} catch (err) {
-// 			return thunkAPI.rejectWithValue('Some Error in saveJsonSettings')
-// 		}
-// }
-// )
+		} catch (err) {
+			dispatch(userActions.setSettingsAwaitingResponse({
+				settings: 'missedTraining',
+				isLoading: false
+			}))
+			dispatch(toastsActions.updateToastById({
+				id: toastId,
+				toast: { status: 'error' }
+			}))
+			return thunkAPI.rejectWithValue('Some Error in saveJsonSettings')
+		}
+	}
+)
 
 // createAsyncThunk принимает несколько дженериков, первый - то что мы возвращаем, второй - то что мы передаем. В качестве третьего аргумента может выступать объект(AsyncThunkConfig), в это объекте можно указать что мы ожидаем от определенных частей thunk. Например, если при ошибки мы хотим возращать определенный тип ошибки, который мы создали самостоятельно, то в качестве AsyncThunkConfig указывает {rejectValue: MyCustomError} и таким образом я типизирую значение, которое возращает thunkAPI.rejectWithValue при ошибке. Это позволяет очень гибко типизировать все входные и выходные значения
--- src/entities/User/model/services/userSettings/updateNotificationThunk.ts
+++ src/entities/User/model/services/userSettings/updateNotificationThunk.ts
@@ -1,41 +1,46 @@
-// import { StateSchema, ThunkExtraArg } from '@/app/providers/StoreProvider'
-// import { createAsyncThunk } from '@reduxjs/toolkit'
-// import { setJsonSettingsMutation } from '../api/userApi'
-// import { getJsonSettings } from '../selectors/getJsonSettings'
-// import { getUserAuthData } from '../selectors/getUserAuthData'
-// import { JsonSettings } from '../types/JsonSettings'
+import { StateSchema, ThunkExtraArg } from '@/app/providers/StoreProvider'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
-// // createAsyncThunk третьим аргументом принимает конфиг и там я могу описать поле extra и теперь обращаясь в thunkAPI.extra ТС подхватит то, что я описал в ThunkExtraArg
-// export const saveJsonSettings = createAsyncThunk<JsonSettings, JsonSettings, { rejectValue: string, extra: ThunkExtraArg, state: StateSchema }>(
-// 	'user/saveJsonSettings',
-// 	async (newJsonSettings, thunkAPI) => {
+import { NotificationSettings } from '../../types/userSettings'
+import { rtkApiUpdateNotifications } from '../../api/userApi'
+import { toastsActions } from '@/shared/ui/Toast'
+import { t } from 'i18next'
+import { userActions } from '../../slice/userSlice'
 
-// 		const { dispatch, getState, } = thunkAPI
-// 		const userData = getUserAuthData(getState())
-// 		const currentUserSettings = getJsonSettings(getState())
+// createAsyncThunk третьим аргументом принимает конфиг и там я могу описать поле extra и теперь обращаясь в thunkAPI.extra ТС подхватит то, что я описал в ThunkExtraArg
+export const updateNotificationSettingsThunk = createAsyncThunk<NotificationSettings, NotificationSettings, { rejectValue: string, extra: ThunkExtraArg, state: StateSchema }>(
+	'user/updateNotificationSettingsThunk',
+	async (notificationSettings, thunkAPI) => {
+		const { dispatch, } = thunkAPI
+		dispatch(userActions.setSettingsAwaitingResponse({ settings: 'notifications', isLoading: true }))
+		dispatch(toastsActions.addToast({
+			id: 'update-notification',
+			toast: {
+				status: 'pending',
+				messageLoading: t('toast:messageLoading'),
+				messageError: t('toast:messageError'),
+				messageSuccess: t('toast:update_settings.messageSuccess'),
+			}
+		}))
+		try {
+			const response = await dispatch(rtkApiUpdateNotifications(notificationSettings)).unwrap()
 
-// 		if (!userData) return thunkAPI.rejectWithValue('Нет userData')
+			dispatch(toastsActions.updateToastById({
+				id: 'update-notification',
+				toast: { status: 'success' }
+			}))
+			dispatch(userActions.setSettingsAwaitingResponse({ settings: 'notifications', isLoading: false }))
+			return response
 
-// 		try {
-// 			const response = await dispatch(setJsonSettingsMutation({
-// 				jsonSettings: {
-// 					...currentUserSettings,
-// 					...newJsonSettings
-// 				},
-// 				userId: userData.id
-// 			}))
-// 				.unwrap() //разворачиваю в реальный результат
+		} catch (err) {
+			thunkAPI.dispatch(toastsActions.updateToastById({
+				id: 'update-notification',
+				toast: { status: 'error' }
+			}))
+			dispatch(userActions.setSettingsAwaitingResponse({ settings: 'notifications', isLoading: false }))
+			return thunkAPI.rejectWithValue('Some Error in saveJsonSettings')
+		}
+	}
+)
 
-// 			const jsonSettingsFromResponse = response.jsonSettings
-
-// 			if (!jsonSettingsFromResponse) return thunkAPI.rejectWithValue('Нет userData')
-
-// 			return jsonSettingsFromResponse
-
-// 		} catch (err) {
-// 			return thunkAPI.rejectWithValue('Some Error in saveJsonSettings')
-// 		}
-// 	}
-// )
-
-// // createAsyncThunk принимает несколько дженериков, первый - то что мы возвращаем, второй - то что мы передаем. В качестве третьего аргумента может выступать объект(AsyncThunkConfig), в это объекте можно указать что мы ожидаем от определенных частей thunk. Например, если при ошибки мы хотим возращать определенный тип ошибки, который мы создали самостоятельно, то в качестве AsyncThunkConfig указывает {rejectValue: MyCustomError} и таким образом я типизирую значение, которое возращает thunkAPI.rejectWithValue при ошибке. Это позволяет очень гибко типизировать все входные и выходные значения
+// createAsyncThunk принимает несколько дженериков, первый - то что мы возвращаем, второй - то что мы передаем. В качестве третьего аргумента может выступать объект(AsyncThunkConfig), в это объекте можно указать что мы ожидаем от определенных частей thunk. Например, если при ошибки мы хотим возращать определенный тип ошибки, который мы создали самостоятельно, то в качестве AsyncThunkConfig указывает {rejectValue: MyCustomError} и таким образом я типизирую значение, которое возращает thunkAPI.rejectWithValue при ошибке. Это позволяет очень гибко типизировать все входные и выходные значения
