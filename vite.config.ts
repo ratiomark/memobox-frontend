df6ac5073db64654cfdf126dc262459963381ae5
ratiomark <yanagae@gmail.com>
2023-12-27 21:28:22 +0200

    update rollup options, optimize bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import visualizer from 'rollup-plugin-visualizer'
+import path from 'path'
 // https://vitejs.dev/config/
 // https://stackoverflow.com/questions/73273017/when-i-run-vite-preview-i-get-the-bundle-size-of-300kb-and-when-i-run-vite-build
 export default defineConfig({
@@ -32,12 +33,16 @@ export default defineConfig({
 	},
 	build: {
 		sourcemap: 'hidden',
+		// sourcemap: 'hidden',
 		rollupOptions: {
 			output: {
 				manualChunks(id) {
-					if (id.includes('/lexical-playground/')) {
-						return 'lexical';
+					if (id.includes('/LexicalEditor/')) {
+						return 'lexical-editor-chunk';
 					}
+					// 	// if (id.includes(path.resolve(__dirname, 'src/shared/ui/LexicalEditor/'))) {
+					// 	// 	return 'lexical-playground-chunk';
+					// 	// }
 				}
 			}
 		}
