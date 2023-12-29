211d5ba616db2b953aa4e8cf1fe7c6b86f3ffc76
ratiomark <yanagae@gmail.com>
2023-12-29 05:20:36 +0200

    remove unused code

--- src/widgets/TrainingWrapper/ui/TrainingWrapper.tsx
+++ src/widgets/TrainingWrapper/ui/TrainingWrapper.tsx
@@ -1,26 +1,13 @@
-import clsx from 'clsx';
-import { useTranslation } from 'react-i18next';
 import cls from './TrainingWrapper.module.scss';
 import { Training } from '@/features/Training';
 import { useParams } from 'react-router-dom';
 
-interface TrainingWrapperProps {
-	className?: string
-}
 
-export const TrainingWrapper = (props: TrainingWrapperProps) => {
-	const {
-		className
-	} = props
+export const TrainingWrapper = () => {
 	const { shelfId = 'all', boxId = 'all' } = useParams()
 
-	const { t } = useTranslation()
-
 	return (
-		<div className={clsx(
-			cls.TrainingWrapper,
-			className)}
-		>
+		<div className={cls.TrainingWrapper}>
 			<Training
 				boxId={boxId}
 				shelfId={shelfId}
