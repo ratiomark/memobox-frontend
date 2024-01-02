eebde0b34e789cc45de82642a0f2554ef4b0fa6d
ratiomark <yanagae@gmail.com>
2024-01-03 00:46:36 +0200

    add cupboard shelf list mobile

---
+++ src/features/CupboardShelfList/ui/CupboardShelfListMobile.tsx
@@ -0,0 +1,152 @@
+import { useTranslation } from 'react-i18next';
+import cls from './CupboardShelfList.module.scss';
+import { useAppDispatch } from '@/shared/lib/helpers/hooks/useAppDispatch';
+import { useCallback, useEffect, useLayoutEffect, useMemo } from 'react';
+import { fetchCupboardDataThunk } from '../model/services/fetchCupboardDataThunk';
+import { useSelector } from 'react-redux';
+import {
+	getCupboardData,
+	getCupboardIsLoading,
+	getCupboardError,
+	getCupboardShelves,
+	getCupboardCommonShelfCollapsed,
+	getCupboardIsDataAlreadyInStore,
+	getCupboardIsFirstRender,
+	// getShelvesFromStorOrLocalSaver,
+} from '../model/selectors/getCupboardShelfList';
+import { cupboardShelfListActions, getCupboardState } from '../model/slice/cupboardShelfListSlice';
+import { ShelfItem } from './ShelfItem/ShelfItem';
+import { ShelfButtons } from './ShelfButtons/ShelfButtons';
+import { CompleteSmallDataLabels } from '@/shared/ui/DataLabels/CompleteSmallDataLabels/CompleteSmallDataLabels';
+import { ShelfSchema } from '@/entities/Shelf';
+import { CommonShelf } from './CommonShelf/CommonShelf';
+import { BoxesBlockWrapper } from './BoxesBlock/BoxesBlockWrapper';
+import { MissedTrainingSettingsModal } from './Modals/MissedTrainingSettingsModal/MissedTrainingSettings';
+import { NotificationSettingsModal } from './Modals/NotificationSettingsModal/NotificationSettingsModal';
+import { BoxTimeSetterModal } from './Modals/BoxTimeSetterModal/BoxTimeSetterModal';
+import { CreateNewCardModal } from './Modals/CreateNewCardModal/CreateNewCardModalLazy';
+// import CreateNewCardModal  from './Modals/CreateNewCardModal/CreateNewCardModal';
+import { AnimateSkeletonLoader } from '@/shared/ui/Animations';
+import { ShelfButtonsSkeleton } from './ShelfButtons/ShelfButtonsSkeleton';
+import { BoxSettingsDropdownModal } from './Modals/BoxSettingsDropdownModal/BoxSettingsDropdownModal';
+import { HiddenTemplates } from './HiddenTemplates/HiddenTemplates';
+import { idCupboardShelfList } from '@/shared/const/idsAndDataAttributes';
+import { CupboardInfoModal } from './Modals/CupboardInfoModal/CupboardInfoModal';
+import { Reorder } from 'framer-motion';
+import { ShelfBoxesTemplateModal } from './BoxesSettingsModal/ShelfTemplateSettingsLazy';
+import useShelvesDndHandler from '../model/hooks/useShelvesDndHandler';
+import useShelvesLocalSaver from '../model/hooks/useShelvesLocalSaver';
+import useCupboardButtonsSizes from '../model/hooks/useCupboardButtonsSizes';
+import { setLocalShelvesToStore } from '../model/services/setLocalShelvesToStore';
+import { RenameShelfModal } from './Modals/RenameShelfModal/RenameShelfModal';
+
+export const CupboardShelfListMobile = () => {
+	const dispatch = useAppDispatch()
+	const cupboardData = useSelector(getCupboardData)
+	const cupboardIsLoading = useSelector(getCupboardIsLoading)
+	const cupboardError = useSelector(getCupboardError)
+	const cupboardShelves = useSelector(getCupboardState.selectAll)
+	const isFirstRender = useSelector(getCupboardIsFirstRender)
+	useShelvesDndHandler()
+	useShelvesLocalSaver({ cupboardShelves })
+	useCupboardButtonsSizes(cupboardIsLoading)
+
+	useEffect(() => {
+		dispatch(setLocalShelvesToStore())
+	}, [dispatch])
+
+	const onAddNewCardClick = useCallback((shelfId: string) => {
+		dispatch(cupboardShelfListActions.setShelfIdCardModal(shelfId))
+		dispatch(cupboardShelfListActions.setIsCreateNewCardModalOpen(true))
+	}, [dispatch])
+
+	const onCollapseClick = useCallback((shelfId: string, isCollapsed: boolean) => {
+		dispatch(cupboardShelfListActions.updateShelf({ id: shelfId, changes: { isCollapsed } }))
+	}, [dispatch])
+
+	const reorderShelves = useCallback((shelves: ShelfSchema[]) => {
+		// console.log('reorder ', shelves)
+		dispatch(cupboardShelfListActions.reorderShelves(shelves))
+		// VAR: нужно показать плашку "новый порядок полок будет сохранен через 5 секунд". По прошествии 5-ти секунд отправить запрос на сервер
+	}, [dispatch])
+
+
+	const shelvesList = useMemo(() => {
+
+		return cupboardShelves.map(shelf => {
+			const completeSmallDataLabels =
+				<CompleteSmallDataLabels
+					data={shelf.data}
+					isLoading={cupboardIsLoading}
+				/>
+			const buttons = (
+				<AnimateSkeletonLoader
+					classNameForCommonWrapper={cls.commonWrapper}
+					skeletonComponent={<ShelfButtonsSkeleton />}
+					componentAfterLoading={<ShelfButtons
+						shelf={shelf}
+						onAddNewCardClick={onAddNewCardClick}
+						onCollapseClick={onCollapseClick}
+					/>}
+					noDelay={!cupboardIsLoading}
+					isLoading={cupboardIsLoading}
+				/>
+			)
+			const boxesBlock = <BoxesBlockWrapper isLoading={cupboardIsLoading} shelf={shelf} />
+
+			return (
+				<ShelfItem
+					isFirstRender={isFirstRender}
+					key={shelf.id}
+					shelf={shelf}
+					boxesBlock={boxesBlock}
+					// moveShelf={moveShelf}
+					completeSmallDataLabelsBlock={
+						completeSmallDataLabels
+					}
+					shelfButtonsBlock={buttons}
+				/>
+			)
+		})
+
+	}, [cupboardIsLoading, isFirstRender, cupboardShelves, onAddNewCardClick, onCollapseClick])
+	// }, [cupboardIsLoading, isFirstRender, cupboardShelves, onAddNewCardClick, onCollapseClick, moveShelf])
+
+	return (
+		<div>
+			<h1>Page</h1>
+			<RenameShelfModal />
+			<CreateNewCardModal />
+		</div>
+		// <>
+		// 	<div
+		// 		className={cls.cupboardShelfList}
+		// 		id={idCupboardShelfList}
+		// 	>
+		// 		<CommonShelf data={cupboardData} isLoading={cupboardIsLoading} />
+		// 		<Reorder.Group
+		// 			// layoutScroll
+		// 			// layout
+		// 			// layoutRoot
+		// 			values={cupboardShelves}
+		// 			// принимает массив с элементами в текущем порядке
+		// 			onReorder={reorderShelves}
+		// 		>
+		// 			{shelvesList}
+		// 		</Reorder.Group>
+		// 		<ShelfBoxesTemplateModal />
+		// 		<MissedTrainingSettingsModal />
+		// 		<NotificationSettingsModal />
+		// 		<BoxSettingsDropdownModal />
+		// 		<CupboardInfoModal />
+		// 		{/* Важно HiddenTemplates должен стоять выше BoxTimeSetterModal там происходят определенные вычисления*/}
+		// 		<HiddenTemplates />
+		// 		<BoxTimeSetterModal />
+		// 	</div>
+		// 	<RenameShelfModal />
+		// 	<CreateNewCardModal />
+		// 	{/* <ShelvesDeletionToasts/> */}
+		// 	{/* <ToastShelfDeletion /> */}
+		// </>
+	)
+}
--- src/features/CupboardShelfList/ui/CupboardShelfListWrapper/CupboardShelfListWrapper.tsx
+++ src/features/CupboardShelfList/ui/CupboardShelfListWrapper/CupboardShelfListWrapper.tsx
@@ -1,102 +1,124 @@
 import { useSelector } from 'react-redux';
 import { CupboardShelfList } from '../CupboardShelfList';
-import { getCupboardIsDataAlreadyInStore, getCupboardIsFirstRender, getCupboardIsLoading } from '../../model/selectors/getCupboardShelfList';
+import { getCupboard, getCupboardIsDataAlreadyInStore, getCupboardIsFirstRender, getCupboardIsLoading } from '../../model/selectors/getCupboardShelfList';
 import { useAppDispatch } from '@/shared/lib/helpers/hooks/useAppDispatch';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useGetCupboardDataQuery } from '@/entities/Cupboard';
-import { cupboardShelfListActions } from '../../model/slice/cupboardShelfListSlice';
+import { cupboardShelfListActions, cupboardShelfListReducer } from '../../model/slice/cupboardShelfListSlice';
 import { fetchCupboardDataThunk } from '../../model/services/fetchCupboardDataThunk';
+import { ReducersList, useAsyncReducer } from '@/shared/lib/helpers/hooks/useAsyncReducer';
+import { getIsMobile } from '@/entities/UI';
+import { CupboardShelfListMobile } from '../CupboardShelfListMobile';
+
+const reducers: ReducersList = {
+	cupboard: cupboardShelfListReducer,
+}
 
 
+
+// const CupboardShelfListPreLoad = () => {
 export const CupboardShelfListWrapper = () => {
+	// useAsyncReducer({ reducers, removeAfterUnmount: false })
 	// const cupboardIsLoading = useSelector(getCupboardIsLoading)
-	const { data, isLoading, error, refetch, isFetching, currentData } = useGetCupboardDataQuery()
+	const { data, isLoading, error, refetch, isFetching, currentData, } = useGetCupboardDataQuery()
 	const isDataAlreadyInStore = useSelector(getCupboardIsDataAlreadyInStore)
 	const isFirstRender = useSelector(getCupboardIsFirstRender)
 	// const isNeedRefetch = useSelector(getCupboardIsNeedRefetch)
 	// const isNeedStop = useSelector(getCupboardIsNeedStop)
 	const dispatch = useAppDispatch()
+	const isMobile = useSelector(getIsMobile)
+
+	// if (!isLoading && !isFetching && data) {
+
+	// 	dispatch(fetchCupboardDataThunk(data))
+	// }
+
+	useEffect(() => {
+		if (!isLoading && !isFetching && data) {
+
+			dispatch(fetchCupboardDataThunk(data))
+		}
+	}, [data, isLoading, isFetching, dispatch])
+
+	useEffect(() => {
+		dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(isLoading))
+	}, [isLoading, dispatch])
+
 	// useEffect(() => {
-	// 	if (data) {
-	// 		dispatch(fetchCupboardDataThunk(data))
+	// 	if (!isFirstRender && isDataAlreadyInStore) {
+	// 		refetch()
+
 	// 	}
-	// }, [dispatch, data])
+
+	// }, [isFirstRender, isDataAlreadyInStore, refetch])
 
 	// useEffect(() => {
-	// 	dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(isLoading))
-	// }, [dispatch, isLoading])
-	// const [refetching, setRefetching] = useState(false)
-	// const counter = useRef(1)
-	// const continueCycle = useRef(true)
-	useEffect(() => {
-		console.log(data, isLoading, error, refetch)
-	}, [data, isLoading, error, refetch])
+	// 	return () => {
+	// 		dispatch(cupboardShelfListActions.setIsFirstRender(false))
+	// 	}
 
-	useEffect(() => {
-		if (!isFirstRender && isDataAlreadyInStore) {
-			refetch()
+	// }, [dispatch])
 
-		}
-
-	}, [isFirstRender, isDataAlreadyInStore, refetch])
-
-	useEffect(() => {
-		return () => {
-			dispatch(cupboardShelfListActions.setIsFirstRender(false))
-		}
-
-	}, [dispatch])
-
-
-	useEffect(() => {
-		// console.log(`Ренденр № ${counter.current}`)
-		// counter.current += 1
-		if (isLoading) {
-			// console.log(`Ренденр № ${counter.current} Загрузка`)
-			// counter.current += 1
-			dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(true))
-		} else if (data) {
-			if (!isDataAlreadyInStore) {
-				// dispatch(cupboardShelfListActions.setFetchedCupboardData(data))
-				dispatch(fetchCupboardDataThunk(data))
-				// dispatch(cupboardShelfListActions.setFetchedCupboardData(data))
-				// return
-			} else if (isFetching) {
-				return
-			} else if (!isFetching) {
-				if (data === currentData) {
-					dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(false))
-					return
-				} else {
-					// dispatch(cupboardShelfListActions.setFetchedCupboardData(data))
-					dispatch(fetchCupboardDataThunk(data))
-					// dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(false))
-
-				}
-			}
-		} else if (error) {
-			// console.log(`Ренденр № ${counter.current} Ошибка`)
-			// counter.current += 1
-			dispatch(cupboardShelfListActions.setFetchedCupboardDataError(error))
-		}
-
-	}, [dispatch, currentData, data, isLoading, isFetching, error, isDataAlreadyInStore, refetch])
 
 	// useEffect(() => {
+	// 	// console.log(`Ренденр № ${counter.current}`)
+	// 	// counter.current += 1
 	// 	if (isLoading) {
+	// 		// console.log(`Ренденр № ${counter.current} Загрузка`)
+	// 		// counter.current += 1
 	// 		dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(true))
 	// 	} else if (data) {
-	// 		dispatch(cupboardShelfListActions.setFetchedCupboardData(data))
+	// 		if (!isDataAlreadyInStore) {
+	// 			// dispatch(cupboardShelfListActions.setFetchedCupboardData(data))
+	// 			dispatch(fetchCupboardDataThunk(data))
+	// 			// dispatch(cupboardShelfListActions.setFetchedCupboardData(data))
+	// 			// return
+	// 		} else if (isFetching) {
+	// 			return
+	// 		} else if (!isFetching) {
+	// 			if (data === currentData) {
+	// 				dispatch(fetchCupboardDataThunk(data))
+	// 				dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(false))
+	// 				return
+	// 			} else {
+	// 				// dispatch(cupboardShelfListActions.setFetchedCupboardData(data))
+	// 				dispatch(fetchCupboardDataThunk(data))
+	// 				// dispatch(cupboardShelfListActions.setFetchedCupboardDataIsLoading(false))
+
+	// 			}
+	// 		}
 	// 	} else if (error) {
+	// 		// console.log(`Ренденр № ${counter.current} Ошибка`)
+	// 		// counter.current += 1
 	// 		dispatch(cupboardShelfListActions.setFetchedCupboardDataError(error))
 	// 	}
 
-	// }, [dispatch, data, isLoading, error,])
-	// useEffect(() => {
-	// 	dispatch(fetchCupboardDataThunk())
-	// }, [dispatch])
+	// }, [dispatch, currentData, data, isLoading, isFetching, error, isDataAlreadyInStore, refetch])
+
+	if (isMobile) {
+		return <CupboardShelfListMobile />
+	}
 	return <CupboardShelfList />
 }
+
+// export const CupboardShelfListWrapper2 = () => {
+// 	const { store } = useAsyncReducer({ reducers, removeAfterUnmount: false })
+// 	const mountedReducers = store.reducerManager.getMountedReducers()
+// 	const reducersMap = store.reducerManager.getReducerMap()
+// 	const isCupboardMounted = useSelector(getCupboard)
+// 	if (isCupboardMounted) {
+// 		// console.log(reducersMap)
+// 		// console.log('mountedReducers', mountedReducers)
+// 		// console.log('mountedReducers.cupboard', mountedReducers.cupboard)
+// 		// console.log(store.reducerManager.getReducerMap())
+// 		return (
+// 			<Suspense fallback={null}>
+// 				<CupboardShelfListPreLoad />
+// 			</Suspense>
+// 		)
+// 	}
+// 	return null
+// }
 
 // import clsx from 'clsx';
 // import { useTranslation } from 'react-i18next';
