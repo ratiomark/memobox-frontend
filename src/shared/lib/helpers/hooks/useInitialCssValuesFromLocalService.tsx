5accde90ca8999ce0240fcf8de614be91cbcfd9b
ratiomark <yanagae@gmail.com>
2024-01-02 23:18:13 +0200

    update var name

--- src/shared/lib/helpers/hooks/useInitialCssValuesFromLocalService.tsx
+++ src/shared/lib/helpers/hooks/useInitialCssValuesFromLocalService.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { localDataService } from '../common/localDataService';
 import { getAppRoot } from '../DOM/getAppRoot';
-import { css_variable_view_rows } from '@/shared/const/cssVariables';
+import { CSS_VAR_view_rows } from '@/shared/const/cssVariables';
 
 export const useInitialCssValuesFromLocalService = () => {
 	useEffect(() => {
 		const root = getAppRoot()
-		root.style.setProperty(css_variable_view_rows, localDataService.getViewRows().toString() ?? '2')
+		root.style.setProperty(CSS_VAR_view_rows, localDataService.getViewRows().toString() ?? '2')
 	}, [])
 }
