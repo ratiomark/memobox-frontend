0b1b6c81ad153cdcb8660496a42168df586dc09c
ratiomark <yanagae@gmail.com>
2024-01-03 00:28:43 +0200

    add: getIsMobile selector

--- src/entities/UI/index.ts
+++ src/entities/UI/index.ts
@@ -1,3 +1,4 @@
 export { getUIScrollByPath } from './model/selectors/getScrollSave';
+export { getIsMobile } from './model/selectors/getIsMobile';
 export { uiActions, uiReducer } from './model/slice/scrollSaveSlice';
 export type { UISchema } from './model/types/ScrollSaveSchema'
