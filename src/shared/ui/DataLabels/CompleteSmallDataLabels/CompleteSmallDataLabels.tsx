1ee3e2a21be915ef4ebf9856063e87c0254cdb26
ratiomark <yanagae@gmail.com>
2024-01-05 06:33:13 +0200

    ref: add isRefetchingSelectorFn props

--- src/shared/ui/DataLabels/CompleteSmallDataLabels/CompleteSmallDataLabels.tsx
+++ src/shared/ui/DataLabels/CompleteSmallDataLabels/CompleteSmallDataLabels.tsx
@@ -2,6 +2,8 @@ import clsx from 'clsx';
 import { memo } from 'react';
 import { SmallDataLabel } from '../SmallDataLabels/SmallDataLabel';
 import cls from './CompleteSmallDataLabels.module.scss';
+import { StateSchema } from '@/app/providers/StoreProvider';
+import { useSelector } from 'react-redux';
 
 interface CompleteSmallDataLabelsProps {
 	className?: string
@@ -10,15 +12,26 @@ interface CompleteSmallDataLabelsProps {
 		all: number
 		train: number
 	}
-	isLoading: boolean
+	isLoading?: boolean
+	isRefetchingSelectorFn?: (state: StateSchema) => boolean
 }
 
 export const CompleteSmallDataLabels = memo((props: CompleteSmallDataLabelsProps) => {
 	const {
 		className,
 		data,
-		isLoading
+		isLoading = true,
+		isRefetchingSelectorFn
 	} = props
+
+	if (isRefetchingSelectorFn) {
+		return (
+			<CompleteSmallDataLabelsWithSelector
+				data={data}
+				className={className}
+				isRefetchingSelectorFn={isRefetchingSelectorFn}
+			/>)
+	}
 
 	return (
 		<div className={clsx(
@@ -31,4 +44,33 @@ export const CompleteSmallDataLabels = memo((props: CompleteSmallDataLabelsProps
 		</div>
 	)
 })
+
+interface CompleteSmallDataLabelsWithSelectorProps {
+	className?: string
+	data?: {
+		wait: number
+		all: number
+		train: number
+	}
+	isRefetchingSelectorFn: (state: StateSchema) => boolean
+}
+const CompleteSmallDataLabelsWithSelector = (props: CompleteSmallDataLabelsWithSelectorProps) => {
+	const {
+		className,
+		data,
+		isRefetchingSelectorFn,
+	} = props
+	const isLoading = useSelector(isRefetchingSelectorFn)
+	return (
+		<div className={clsx(
+			cls.CompleteSmallDataLabels,
+			[className])}
+		>
+			<SmallDataLabel isLoading={isLoading} cardsCount={data?.all} type='all' />
+			<SmallDataLabel isLoading={isLoading} cardsCount={data?.train} type='train' />
+			<SmallDataLabel isLoading={isLoading} cardsCount={data?.wait} type='wait' />
+		</div>
+	)
+}
+
 CompleteSmallDataLabels.displayName = 'CompleteSmallDataLabels'
