89df3ebacdce9a94a370d2cf8e8bd98ead47af19
ratiomark <yanagae@gmail.com>
2024-01-08 22:17:57 +0200

    Add selector for checking if box is a learnt box

--- src/features/CupboardShelfList/model/selectors/getBoxSettingDropdownModal.ts
+++ src/features/CupboardShelfList/model/selectors/getBoxSettingDropdownModal.ts
@@ -1,8 +1,24 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
+import { createSelector } from '@reduxjs/toolkit';
+import { getCupboardState } from '../..';
 
 export const getBoxSettingsDropdownModalIsOpen = (state: StateSchema) => state.cupboard.boxSettingsDropdownModal.isOpen
 export const getBoxSettingsDropdownCoordinates = (state: StateSchema) => state.cupboard.boxSettingsDropdownModal.boxCoordinates
 export const getBoxSettingsDropdownBoxId = (state: StateSchema) => state.cupboard.boxSettingsDropdownModal.boxId
+export const getBoxSettingsDropdownShelfId = (state: StateSchema) => state.cupboard.boxSettingsDropdownModal.shelfId
 
+export const getBoxSettingsDropdownIsLearntBox = createSelector(
+	[
+		(state: StateSchema) => state,
+		getBoxSettingsDropdownBoxId,
+		getBoxSettingsDropdownShelfId,
+	],
+	(state, boxId, shelfId) => {
+		const shelf = getCupboardState.selectById(state, shelfId)
+		if(!shelf) return false
+		const boxesData = shelf.boxesData
+		return boxesData.find((box) => box.id === boxId)?.specialType === 'learnt'
+	}
+)
 
 // export const getDeletionIds = (state: StateSchema) => state.cupboard.shelvesDeletionIds 
