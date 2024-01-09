01dd93bf29393bd41506e1b01a971100c0423527
ratiomark <yanagae@gmail.com>
2024-01-09 15:26:29 +0200

    Add selectors for shelf and box deletion process

---
+++ src/features/CupboardShelfList/model/selectors/getDeletionProcess.ts
@@ -0,0 +1,50 @@
+import { StateSchema } from '@/app/providers/StoreProvider';
+import { EntityId, createSelector } from '@reduxjs/toolkit';
+import { getCupboardState } from '../..';
+import { RequestStatusType } from '@/shared/types/GeneralTypes';
+
+// export const getShelfDeletionRequestStatus = (state: StateSchema) => state.cupboard.shelfDeletionProcess.requestStatus
+// export const getShelfDeletionRequestStatusByShelfId = (state: StateSchema) => state.cupboard.shelfDeletionProcess.requestStatus
+export const getShelfDeletionShelfId = (state: StateSchema) => state.cupboard.shelfDeletionProcess.shelfId
+export const getShelfDeletionIsAnyShelfInDeletionProcess = (state: StateSchema) => state.cupboard.shelfDeletionProcess.isAnyShelfInDeletionProcess ?? false
+
+export const getShelfIsDeleting = (shelfId: string) => {
+	return createSelector(
+		[
+			(state: StateSchema) => state
+		],
+		(state) => getCupboardState.selectById(state, shelfId)?.isDeleting
+	)
+}
+
+export const getBoxIsDeleting = (shelfId: string, boxId: string) => {
+	return createSelector(
+		[
+			(state: StateSchema) => state
+		],
+		(state) => {
+			const shelf = getCupboardState.selectById(state, shelfId)
+			if (!shelf) return false
+			return shelf.boxesData.find(box => box.id === boxId)?.isDeleting
+		}
+	)
+}
+
+export const getShelfIsDeleted = (shelfId: string) => {
+	return createSelector(
+		[
+			(state: StateSchema) => state
+		],
+		(state) => getCupboardState.selectById(state, shelfId)?.isDeleted
+	)
+}
+
+export const getShelfDeletionRequestStatus = (id: string | EntityId) => {
+	return createSelector(
+		[
+			(state: StateSchema) => state
+		],
+		(state) => getCupboardState.selectById(state, id)?.deletingRequestStatus ?? 'idle' as RequestStatusType
+	)
+}
+
