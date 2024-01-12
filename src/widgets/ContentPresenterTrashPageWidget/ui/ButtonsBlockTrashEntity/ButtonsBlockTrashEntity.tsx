0acfca778adae75f82324e925dad81c45cd7f516
ratiomark <yanagae@gmail.com>
2024-01-12 14:02:36 +0200

    refactor component

--- src/widgets/ContentPresenterTrashPageWidget/ui/ButtonsBlockTrashEntity/ButtonsBlockTrashEntity.tsx
+++ src/widgets/ContentPresenterTrashPageWidget/ui/ButtonsBlockTrashEntity/ButtonsBlockTrashEntity.tsx
@@ -1,61 +1,73 @@
-import TrashIcon from '@/shared/assets/icons/trashIcon2.svg'
+import React from 'react';
+import TrashIcon from '@/shared/assets/icons/trashIcon2.svg';
 import ArrowBottomIcon from '@/shared/assets/icons/arrow-bottom.svg';
-import cls from './ButtonsBlockTrashEntity.module.scss'
+import cls from './ButtonsBlockTrashEntity.module.scss';
 import clsx from 'clsx';
 import { Button } from '@/shared/ui/Button';
 import { Icon } from '@/shared/ui/Icon';
 import { useTranslation } from 'react-i18next';
 
-interface ButtonsBlockProps {
-	isCollapsed: boolean
-	onCollapseClick: () => void
-	onRestoreClick?: () => void
-	onRemoveClick?: () => void
-	preserveSpaceForCollapseArrow?: boolean
-	noCollapseArrow?: boolean
+export interface ButtonsBlockTrashEntityProps {
+	isCollapsed: boolean;
+	onToggleCollapse?: () => void;
+	onRestore?: () => void;
+	onRemove?: () => void;
+	showCollapseArrow?: boolean;
+	showRestoreButton?: boolean;
+	showRemoveButton?: boolean;
+	removeRestoreButton?: boolean; // Новый пропс для удаления кнопки "Restore"
+	removeRemoveButton?: boolean; // Новый пропс для удаления кнопки "Remove"
 }
 
-export const ButtonsBlockTrashEntity = (props: ButtonsBlockProps) => {
-	const {
-		isCollapsed = true,
-		onCollapseClick,
-		onRestoreClick = () => { alert('restore') },
-		onRemoveClick = () => { alert('remove') },
-		preserveSpaceForCollapseArrow = false,
-		noCollapseArrow = false
-	} = props
-	const { t } = useTranslation('trash-page')
-
-
+export const ButtonsBlockTrashEntity: React.FC<ButtonsBlockTrashEntityProps> = ({
+	isCollapsed,
+	onToggleCollapse = () => alert('onToggleCollapse'),
+	onRestore = () => alert('onRestore'),
+	onRemove = () => alert('onRemove'),
+	showCollapseArrow = true,
+	showRestoreButton = true,
+	showRemoveButton = true,
+	removeRestoreButton = false,
+	removeRemoveButton = false,
+}) => {
+	const { t } = useTranslation('trash-page');
 
 	return (
-		<div className={cls.buttons} >
-			<Button
-				onClick={onRestoreClick}
-			>
-				{t('restore')}
-			</Button>
-			<Icon
-				Svg={TrashIcon}
-				type='cancel'
-				clickable
-				withFill={false}
-				width={22}
-				height={22}
-				onClick={onRemoveClick}
-				buttonSameSize={false}
-				className={clsx(cls.icon, cls.removeIcon)}
-			/>
-			{noCollapseArrow && preserveSpaceForCollapseArrow && <div style={{ width: 32, height: 32, opacity: 0 }}></div>}
-			{!noCollapseArrow && <Icon
-				className={
-					clsx(cls.arrow, !isCollapsed ? cls.rotateArrow : '')}
-				clickable
-				type='hint'
-				Svg={ArrowBottomIcon}
-				onClick={onCollapseClick}
-			/>}
-
-		</div>)
-
-}
+		<div className={cls.buttons}>
+			{!removeRestoreButton && (
+				<Button
+					onClick={onRestore}
+					style={{ visibility: showRestoreButton ? 'visible' : 'hidden' }}
+				>
+					{t('restore')}
+				</Button>
+			)}
+			{!removeRemoveButton && (
+				<Icon
+					Svg={TrashIcon}
+					type='cancel'
+					clickable={showRemoveButton}
+					withFill={false}
+					width={22}
+					height={22}
+					onClick={showRemoveButton ? () => { } : onRemove}
+					buttonSameSize={false}
+					className={clsx(cls.icon, showRemoveButton ? cls.removeIcon : '')}
+					style={{ visibility: showRemoveButton ? 'visible' : 'hidden' }}
+				/>
+			)}
+			{showCollapseArrow && (
+				<Icon
+					className={clsx(cls.arrow, !isCollapsed ? cls.rotateArrow : '')}
+					clickable={onToggleCollapse !== undefined}
+					type='hint'
+					Svg={ArrowBottomIcon}
+					onClick={onToggleCollapse}
+				/>
+			)}
+			{(!showCollapseArrow && !removeRestoreButton && !removeRemoveButton) && (
+				<div style={{ width: 32, height: 32 }} /> // Место занимается, но кнопка не показывается
+			)}
+		</div>
+	);
+};
