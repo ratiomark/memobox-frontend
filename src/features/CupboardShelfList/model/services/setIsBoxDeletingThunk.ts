bfc9e98854fc81be63f82a888ffbe934cb1cbfd6
ratiomark <yanagae@gmail.com>
2024-01-12 02:49:32 +0200

    Fix setIsBoxDeletingThunk props

--- src/features/CupboardShelfList/model/services/setIsBoxDeletingThunk.ts
+++ src/features/CupboardShelfList/model/services/setIsBoxDeletingThunk.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { StateSchema, ThunkExtraArg } from '@/app/providers/StoreProvider'
-import { BoxSchema,} from '@/entities/Box'
+import { BoxSchema, } from '@/entities/Box'
 import { getBoxSettingsDropdownBoxId, getBoxSettingsDropdownShelfId } from '../selectors/getBoxSettingDropdownModal'
 import { DataBlock } from '@/shared/types/DataBlock'
 import { cupboardShelfListActions } from '../slice/cupboardShelfListSlice'
@@ -11,13 +11,15 @@ export type DeleteBoxThunkResponse = {
 	boxCardsData: DataBlock
 	shelfCardsData: DataBlock
 }
-
-export const setIsBoxDeletingThunk = createAsyncThunk<void, boolean, { rejectValue: string; extra: ThunkExtraArg; state: StateSchema; }>(
-	'cupboardPage/deleteBoxThunk',
-	async (isDeleting, thunkAPI) => {
+interface BoxDeletingThunkProps {
+	boxId: string
+	isDeleting: boolean
+}
+export const setIsBoxDeletingThunk = createAsyncThunk<void, BoxDeletingThunkProps, { rejectValue: string; extra: ThunkExtraArg; state: StateSchema; }>(
+	'cupboardPage/setIsBoxDeletingThunk',
+	async ({ isDeleting, boxId }, thunkAPI) => {
 		const { dispatch, getState } = thunkAPI
 		try {
-			const boxId = getBoxSettingsDropdownBoxId(getState())
 			const shelfId = getBoxSettingsDropdownShelfId(getState())
 			if (!boxId || !shelfId) return thunkAPI.rejectWithValue('some error')
 
