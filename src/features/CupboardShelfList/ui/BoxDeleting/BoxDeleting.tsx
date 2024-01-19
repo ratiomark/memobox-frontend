5efc3b5f602a6c4b32ad51ef3674016a3832f640
ratiomark <yanagae@gmail.com>
2024-01-20 00:38:36 +0200

    ref: remove unused code

--- src/features/CupboardShelfList/ui/BoxDeleting/BoxDeleting.tsx
+++ src/features/CupboardShelfList/ui/BoxDeleting/BoxDeleting.tsx
@@ -2,15 +2,15 @@
 // eslint-disable-next-line custom-fsd-checker-plugin/public-api-imports
 import cls from '@/entities/Box/ui/Box.module.scss';
 import { useTranslation } from 'react-i18next';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback } from 'react';
 import { useAppDispatch } from '@/shared/lib/helpers/hooks/useAppDispatch';
-import { cupboardShelfListActions } from '../..';
+import { cupboardShelfListActions } from '../../model/slice/cupboardShelfListSlice';
 import { Heading, } from '@/shared/ui/Typography';
 import { Button } from '@/shared/ui/Button';
 import { DURATION_SHELF_DELETION_MILLISEC } from '@/shared/const/animation';
 import { CircularCountDownWithProgress } from '@/shared/ui/CircularCountDownWithProgress';
 import { useDeleteWithCountdown } from '@/shared/lib/helpers/hooks/useDeleteWithCountdown';
-import { idPrefixBoxDeletion, idPrefixShelfDeletion } from '@/shared/const/idsAndDataAttributes';
+import { idPrefixBoxDeletion } from '@/shared/const/idsAndDataAttributes';
 import { setIsBoxDeletingThunk } from '../../model/services/setIsBoxDeletingThunk';
 import { deleteBoxThunk } from '../../model/services/deleteBoxThunk';
 
@@ -41,11 +41,10 @@ export const BoxDeleting = (props: BoxDeletingProps) => {
 
 	const onCancelDeletion = async () => {
 		if (timer) clearTimeout(timer)
-		dispatch(setIsBoxDeletingThunk(false))
+		dispatch(setIsBoxDeletingThunk({ boxId, isDeleting: false}))
 		dispatch(cupboardShelfListActions.setAbortedThunkId(idPrefixBoxDeletion + boxId))
 	}
 
-	// const title = <Heading as='h5' className={cls.title} title={index} />
 	return (
 		<li className={cls.Box} >
 			<div className={cls.boxInnerWrapperDeletion} >
@@ -57,30 +56,6 @@ export const BoxDeleting = (props: BoxDeletingProps) => {
 				<div style={{ width: 130 }} />
 			</div>
 		</li>)
-
-	// return (
-
-	// <div className={clsx(
-	// 	cls.ShelfDeleting,
-	// )}
-	// >
-	// 	<VStack align='start' gap='gap_8'>
-	// 		<Heading as='h3' size='s' title={index.toString()} className={cls.title} />
-	// 		<MyText className={cls.description} text={t('shelf in deletion proccess')} />
-	// 	</VStack>
-	// 	<Button className={cls.button} fontWeight='300' onClick={onCancelDeletion}>
-	// 		{t('cancel shelf deletion')}
-	// 	</Button>
-	// 	<CircularCountDownWithProgress duration={DURATION_SHELF_DELETION_MILLISEC / 1000} />
-	// 	{/* <div className={cls.countdown}>
-	// 		<svg className={cls.svg} viewBox="-50 -50 100 100" strokeWidth="7">
-	// 			<circle className={cls.first} r="45"></circle>
-	// 			<circle className={cls.first} r="45" pathLength="1"></circle>
-	// 		</svg>
-	// 	</div> */}
-	// </div>
-
-	// )
 }
 
 
