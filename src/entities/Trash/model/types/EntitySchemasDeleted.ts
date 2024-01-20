28ffc90bddc38de6754a22517b6eaf9a351cc472
ratiomark <yanagae@gmail.com>
2024-01-21 01:33:04 +0200

    update types

--- src/entities/Trash/model/types/EntitySchemasDeleted.ts
+++ src/entities/Trash/model/types/EntitySchemasDeleted.ts
@@ -1,6 +1,7 @@
 import { BoxSchema } from '@/entities/Box'
 import { CardSchemaExtended } from '@/entities/Card'
 import { ShelfSchema } from '@/entities/Shelf'
+import { TimingBlock } from '@/shared/types/DataBlock'
 
 export interface CardSchemaDeleted extends CardSchemaExtended {
 	isDeleted: true
@@ -11,14 +12,21 @@ export type BoxSchemaDeleted = BoxSchema & {
 	isDeleted: true
 	deletedAt: string
 	cardsCount?: number
+	card: CardSchemaDeleted[]
+	_count: {
+		card: number
+	}
+	timing: TimingBlock
+	// specialType: 'new' | 'none' | 'learnt'
 	// cards: CardSchemaDeleted[]
 }
 
 export interface ShelfSchemaDeleted extends ShelfSchema {
 	isDeleted: true
 	deletedAt: string
+	box: BoxSchemaDeleted[]
 	boxesCount: number
-	cardsCount: number
 	card: CardSchemaDeleted[]
+	cardsCount: number
 	// timeDeletion
 }
