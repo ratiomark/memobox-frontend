c8252b6b555cab2aa5150e02d8d51e9fd4426d1c
ratiomark <yanagae@gmail.com>
2024-01-22 00:39:09 +0200

    ref: remove unused code

--- src/features/CupboardShelfList/model/services/createNewShelfThunk.ts
+++ src/features/CupboardShelfList/model/services/createNewShelfThunk.ts
@@ -1,14 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { StateSchema, ThunkExtraArg } from '@/app/providers/StoreProvider'
 import { cupboardShelfListActions, getCupboardState } from '../slice/cupboardShelfListSlice'
-// import { createNewShelf } from '@/entities/Cupboard'
 import { ShelfSchema, rtkCreateNewShelf } from '@/entities/Shelf'
 import { genRandomId } from '@/shared/lib/helpers/common/genRandomId'
 import { toastsActions } from '@/shared/ui/Toast'
 import { sleep } from '@/shared/lib/helpers/common/sleep'
 import { t } from 'i18next'
 import { rtkApi } from '@/shared/api/rtkApi'
-import { TAG_CUPBOARD_PAGE, TAG_VIEW_PAGE } from '@/shared/api/const/tags'
+import { TAG_TRASH_PAGE, TAG_VIEW_PAGE } from '@/shared/api/const/tags'
 
 export const createNewShelfThunk = createAsyncThunk<ShelfSchema[], string, { rejectValue: string; extra: ThunkExtraArg; state: StateSchema }>(
 	'cupboardPage/createNewShelfThunk',
@@ -29,18 +28,14 @@ export const createNewShelfThunk = createAsyncThunk<ShelfSchema[], string, { rej
 		)
 		dispatch(cupboardShelfListActions.setCreateNewShelfModalRequestStatus('pending'))
 		try {
-			// dispatch(cupboardShelfListActions.setIsCreateNewShelfModalAwaitingResponse(true))
-			// console.log('shelfName   ', shelfName)
 			const response = await dispatch(rtkCreateNewShelf(shelfName)).unwrap()
 			// await sleep(20)
-			// const response = await dispatch(createNewShelf(shelfName)).unwrap()
 			if (!response) {
 				dispatch(cupboardShelfListActions.setCreateNewShelfModalRequestStatus('error'))
 				dispatch(toastsActions.updateToastById({ id, toast: { status: 'error' } }))
 				throw new Error()
 			}
-			// console.log('НОВАЯ ПОЛКА', response)
-			dispatch(rtkApi.util.invalidateTags([TAG_VIEW_PAGE]))
+			dispatch(rtkApi.util.invalidateTags([TAG_VIEW_PAGE, TAG_TRASH_PAGE]))
 			const shelves = getCupboardState.selectAll(getState()).map((shelf) => ({
 				...shelf,
 				index: shelf.index + 1,
