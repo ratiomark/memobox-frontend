b98d452533d0719354337967480290ec0f79e5de
ratiomark <yanagae@gmail.com>
2024-02-04 22:42:17 +0200

    ref: remove unused code

--- src/widgets/ContentPresenterTrashPageWidget/ui/Modals/RestoreBoxModal/RestoreBoxModal.tsx
+++ src/widgets/ContentPresenterTrashPageWidget/ui/Modals/RestoreBoxModal/RestoreBoxModal.tsx
@@ -40,14 +40,6 @@ export const RestoreBoxModal = memo(() => {
 		dispatch(trashPageActions.setRestoreBoxModalShelfTitle(shelvesItems.find(shelf => shelf.value === shelfId)?.content ?? '???'))
 	}
 
-	const onChangeBoxId = (boxId: string) => {
-		dispatch(viewPageActions.setMoveCardsModalBoxId(boxId))
-	}
-
-	const onCloseMoveCards = () => {
-		dispatch(viewPageActions.setMoveCardsModalIsOpen(false))
-		document.body.blur()
-	}
 
 	const shelves = (
 		<div className={cls.listBoxWrapper}>
@@ -72,7 +64,6 @@ export const RestoreBoxModal = memo(() => {
 			onSubmit={() => { }}
 			onClose={() => dispatch(trashPageActions.setIsRestoreBoxModalOpen(false))}
 			max
-		// className={cls.MoveCardsModal}
 		>
 			{/* <div style={{ margin: 'auto', width: '90%' }}> */}
 			<MyText className={cls.modalTitle} text={t('Выберите полку на которую нужно восстановить коробку')} align='center' />
