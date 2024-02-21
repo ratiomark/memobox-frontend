704c73d1ea367d8a6f6bd3e469905b3c5f2354d7
ratiomark <yanagae@gmail.com>
2024-02-21 23:54:17 +0200

    add: forgot modal

--- src/features/AuthByUsername/Model/selectors/getLoginState/getLoginState.ts
+++ src/features/AuthByUsername/Model/selectors/getLoginState/getLoginState.ts
@@ -1,6 +1,10 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 
 export const getLoginError = (state: StateSchema) => state?.loginForm?.error
+export const getLoginIsLoginProcess = (state: StateSchema) => state?.loginForm?.isLoginProcess || false
 export const getLoginIsLoading = (state: StateSchema) => state?.loginForm?.isLoading || false
 export const getLoginPassword = (state: StateSchema) => state?.loginForm?.password || ''
-export const getLoginUsername = (state: StateSchema) => state?.loginForm?.email || ''
+export const getLoginEmail = (state: StateSchema) => state?.loginForm?.email || ''
+export const getLoginUserName = (state: StateSchema) => state?.loginForm?.name || ''
+
+export const getIsForgotModalOpen = (state: StateSchema) => state?.loginForm?.isForgotModalOpen ?? false
