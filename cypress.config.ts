71216f1e557a5964c5e0d3d21497f338b44ae73f
ratiomark <yanagae@gmail.com>
2024-03-05 23:22:14 +0200

    add: cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,28 +1,43 @@
 import { defineConfig } from 'cypress';
-import vitePreprocessor from 'cypress-vite'
 import { resolve } from 'path'
+import vitePreprocessor from 'cypress-vite'
+import fs from 'fs';
 
 export default defineConfig({
 	e2e: {
 		trashAssetsBeforeRuns: true,
+		video: true,
+		videosFolder: 'cypress/video-test',
 		// screenshotsFolder: 'cypress/screenshots-test',
-		setupNodeEvents(on) {
-			on(
-				'file:preprocessor',
-				vitePreprocessor({
-					// configFile: resolve(__dirname, './vite.config.cypress.ts'),
-					mode: 'development',
-					build: {
-						sourcemap: 'inline',
-					},
-					resolve: {
-						alias: [
-							{ find: '@', replacement: '/src' },
-							{ find: '#', replacement: '/cypress' },
-						]
-					},
-				}),
-			)
+		setupNodeEvents(on, config) {
+			// Настройка препроцессора файлов
+			on('file:preprocessor', vitePreprocessor({
+				// configFile: resolve(__dirname, './vite.config.cypress.ts'),
+				mode: 'development',
+				build: {
+					sourcemap: 'inline',
+				},
+				resolve: {
+					alias: [
+						{ find: '@', replacement: '/src' },
+						{ find: '#', replacement: '/cypress' },
+					]
+				},
+			}));
+
+			// Обработчик для удаления видео, если в тестах нет ошибок
+			on('after:spec', (spec, results) => {
+				if (results && results.video) {
+					// Проверяем, есть ли неудачные попытки выполнения тестов
+					const failures = results.tests.some((test) =>
+						test.attempts.some((attempt) => attempt.state === 'failed')
+					);
+					if (!failures) {
+						// Удаляем видео, если все тесты в спецификации прошли успешно и не было перезапусков
+						fs.unlinkSync(results.video);
+					}
+				}
+			});
 		},
 		env: {
 			NODE_ENV: JSON.stringify('development'),
