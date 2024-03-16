d5418ad2f983681a636f3327ab01594c446aae58
ratiomark <yanagae@gmail.com>
2024-03-16 23:25:50 +0200

    ref: update types according to backend

--- src/features/SettingsFeatures/TimeSleepSettings/model/types/TimeSleepTypes.ts
+++ src/features/SettingsFeatures/TimeSleepSettings/model/types/TimeSleepTypes.ts
@@ -4,8 +4,27 @@ export type SleepMode = 'up' | 'down';
 export type DayType = 'general' | DaysOfWeek;
 export type TimeControllerFunction = (type: DayType, sleepMode: SleepMode) => void
 
-export interface SetTimePayloadAction {
-	dayType: DayType
+export interface SetGeneralTimePayloadAction {
+	dayType: 'general'
 	operation: 'plus' | 'minus',
-	sleepMode: SleepMode
+	// sleepMode: SleepMode
 }
+export interface SetSpecificDayTimePayloadAction {
+	dayType: DaysOfWeek
+	operation: 'plus' | 'minus',
+	indexPeriod: number
+}
+
+export type SetTimePayloadAction = SetGeneralTimePayloadAction | SetSpecificDayTimePayloadAction
+
+export interface SetGeneralDurationPayloadAction {
+	dayType: 'general'
+	durationMinutes: number
+}
+export interface SetSpecificDayDurationPayloadAction {
+	dayType: DaysOfWeek
+	durationMinutes: number
+	indexPeriod: number
+}
+
+export type SetDurationMinutesPayloadAction = SetGeneralDurationPayloadAction | SetSpecificDayDurationPayloadAction
