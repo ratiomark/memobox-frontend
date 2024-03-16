342d0220ef784762295a3057133603423e218087
ratiomark <yanagae@gmail.com>
2024-03-16 23:25:26 +0200

    ref: remove unused code & prettify

--- src/features/SettingsFeatures/TimeSleepSettings/ui/DayByDayHoursMinutesComponent/DayByDayHoursMinutesComponent.tsx
+++ src/features/SettingsFeatures/TimeSleepSettings/ui/DayByDayHoursMinutesComponent/DayByDayHoursMinutesComponent.tsx
@@ -7,33 +7,24 @@ import { MyText } from '@/shared/ui/Typography';
 import { useTranslation } from 'react-i18next';
 import { daysOfWeek } from '@/entities/User';
 
-// interface GeneralHoursMinutesProps {
-// 	className?: string;
-// }
 
-// export const DayByDayHoursMinutesComponent = (props: GeneralHoursMinutesProps) => {
-// const {
-// 	className,
-// } = props
-		
 export const DayByDayHoursMinutesComponent = () => {
-	const generalTimeSleepData = useSelector(getGeneralTimeSleepData)
 	const dayByDayTimeSleepData = useSelector(getDayByDayTimeSleepData)
 	const { t } = useTranslation('settings')
 
-	return (	
-		
+	return (
+
 		<div className={cls.DayByDayHoursMinutesComponent}>
-			<div/>
+			<div />
 			<MyText align='center' text={t('start sleep')} className={cls.title} />
 			<MyText align='center' text={t('end sleep')} className={cls.title} />
 			{daysOfWeek.map(day => {
-				return (					
+				return (
 					<>
 						<MyText text={t(`days.${day}`)} size='s' />
 						<HoursMinutesWrapper
 							dayType={day}
-							timeSleepData={dayByDayTimeSleepData?.[day]}
+							timeSleepData={dayByDayTimeSleepData?.[day][0]}
 						/>
 					</>
 				)
@@ -41,68 +32,3 @@ export const DayByDayHoursMinutesComponent = () => {
 		</div>
 	)
 }
-
-
-// import clsx from 'clsx'
-// import cls from './DayByDayHoursMinutesComponent.module.scss'
-// import { useSelector } from 'react-redux';
-// import { getDayByDayTimeSleepData, getGeneralTimeSleepData } from '../../model/selectors/settingsTimeSleep';
-// import { HoursMinutesWrapper } from '../HoursMinutesWrapper/HoursMinutesWrapper';
-// import { DaysOfWeek } from '@/entities/User';
-// import { MyText } from '@/shared/ui/Typography';
-// import { useTranslation } from 'react-i18next';
-// import { useEffect, useRef } from 'react';
-// import { motion } from 'framer-motion';
-
-// interface GeneralHoursMinutesProps {
-// 	className?: string;
-// }
-// const daysOfWeek: DaysOfWeek[] = [
-// 	'monday',
-// 	'tuesday',
-// 	'wednesday',
-// 	'thursday',
-// 	'friday',
-// 	'saturday',
-// 	'sunday'
-// ];
-// export const DayByDayHoursMinutesComponent = (props: GeneralHoursMinutesProps) => {
-// 	const {
-// 		className,
-// 	} = props
-// 	const dayByDayTimeSleepData = useSelector(getDayByDayTimeSleepData)
-// 	const wrapperRef = useRef<HTMLDivElement>(null)
-
-// 	// useEffect(() => {
-// 	// 	wrapperRef.current!.scrollBy({ top: 100, left: 1000, behavior: 'smooth' })
-// 	// }, [])
-
-// 	const { t } = useTranslation('settings')
-// 	return (
-// 		// <div
-// 		// 	ref={wrapperRef}
-// 		// 	className={clsx(cls.DayByDayHoursMinutesComponent, [className])}
-// 		// >
-// 		<>
-// 			<div className={cls.titles} >
-				
-// 				<MyText text={t('start sleep')} className={cls.title} />
-// 				<MyText text={t('end sleep')} className={cls.title} />
-
-// 			</div>
-// 			{daysOfWeek.map(day => {
-// 				return (
-// 					<div key={day} className={cls.dayWrapper} >
-// 						<MyText text={t(`days.${day}`)} size='s' />
-// 						<HoursMinutesWrapper
-// 							dayType={day}
-// 							timeSleepData={dayByDayTimeSleepData?.[day]}
-// 						/>
-// 					</div>
-// 				)
-// 			})}
-// 		</>
-
-
-// 	)
-// }
