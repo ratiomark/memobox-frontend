411cd5538547a4b947a991238bb6f75d39dd7fc3
ratiomark <yanagae@gmail.com>
2024-03-18 04:59:26 +0200

    add

---
+++ src/shared/const/daysOfWeek.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line custom-fsd-checker-plugin/layer-import-sequence
+import { DaysOfWeek } from '@/entities/User';
+
+export const daysOfWeekListValues: DaysOfWeek[] = [
+	'monday',
+	'tuesday',
+	'wednesday',
+	'thursday',
+	'friday',
+	'saturday',
+	'sunday',
+];
