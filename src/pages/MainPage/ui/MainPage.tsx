24fc2e268646ea3fec2a935e5aa1054f7a8eccd9
ratiomark <yanagae@gmail.com>
2024-03-26 13:01:18 +0200

    ref: add padding on small screen

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -9,6 +9,7 @@ import { useEffect } from 'react'
 // eslint-disable-next-line custom-fsd-checker-plugin/layer-import-sequence
 import { LoginPage } from '@/pages/LoginPage'
 import { TEST_PAGES_IDS } from '@/shared/const/testConsts'
+import cls from './MainPage.module.scss'
 
 const MainPage = () => {
 	const auth = useSelector(getUserAuthData)
@@ -33,9 +34,15 @@ const MainPage = () => {
 
 	// без StatsAndActionsCupboardWidget не будет работать CupboardShelfListWrapper из-за перерасчета ширины кнопок
 	return (
-		<Page data-testid={TEST_PAGES_IDS.mainPage}>
-			<StatsAndActionsCupboardWidget />
-			<CupboardShelfListWrapper />
+		<Page
+			// className={cls.wrapper}
+			data-testid={TEST_PAGES_IDS.mainPage}
+		>
+			<div className={cls.wrapper} >
+
+				<StatsAndActionsCupboardWidget />
+				<CupboardShelfListWrapper />
+			</div>
 		</Page>
 	)
 }
