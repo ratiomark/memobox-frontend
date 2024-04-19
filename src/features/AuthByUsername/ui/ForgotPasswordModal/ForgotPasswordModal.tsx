c28637b5f56757f133156dfc23b54aaf526c54fe
ratiomark <yanagae@gmail.com>
2024-04-19 22:55:28 +0300

    ref: update styles

--- src/features/AuthByUsername/ui/ForgotPasswordModal/ForgotPasswordModal.tsx
+++ src/features/AuthByUsername/ui/ForgotPasswordModal/ForgotPasswordModal.tsx
@@ -8,25 +8,36 @@ import { HDialogHeadless } from '@/shared/ui/HDialog/HDialogHeadless';
 import { useSelector } from 'react-redux';
 import { getIsForgotModalOpen } from '../../Model/selectors/getLoginState/getLoginState';
 import { ModalButtons } from '@/shared/ui/ModalButtons';
-import { MyText } from '@/shared/ui/Typography';
+import { Heading, MyText } from '@/shared/ui/Typography';
 import { Input } from '@/shared/ui/Input/Input';
 import { useState } from 'react';
+import { useForgotPasswordMutation } from '@/entities/User';
+import { set } from 'lodash';
+import { VStack } from '@/shared/ui/Stack';
 
-
-interface CupboardInfoModalProps {
-	className?: string
-}
-const faqDataList = [
-	{ questionKey: 'FAQ question', answerKey: 'FAQ answer' },
-	{ questionKey: 'FAQ question', answerKey: 'FAQ answer' },
-	{ questionKey: 'FAQ question', answerKey: 'FAQ answer' },
-	{ questionKey: 'FAQ question', answerKey: 'FAQ answer' },
-]
 export const ForgotPasswordModal = () => {
 	const { t } = useTranslation()
 	const dispatch = useAppDispatch()
 	const isOpen = useSelector(getIsForgotModalOpen)
 	const [email, setEmail] = useState('')
+	const [forgotPasswordCallback] = useForgotPasswordMutation()
+	const [responseSuccess, setResponseSuccess] = useState<boolean | null>(null)
+
+	const onSendPasswordClick = async () => {
+		try {
+			const res = await forgotPasswordCallback({ email }).unwrap()
+			if (res === null) setResponseSuccess(true)
+		} catch (error) {
+			setResponseSuccess(false)
+		}
+	}
+
+	const onChangeEmail = (email: string) => {
+		setEmail(email)
+		if (responseSuccess !== null) {
+			setResponseSuccess(null)
+		}
+	}
 
 	const onCloseModal = () => {
 		dispatch(loginActions.setIsForgotPasswordModal(false))
@@ -44,21 +55,26 @@ export const ForgotPasswordModal = () => {
 			<div
 				className={cls.cardModal}
 			>
-				<MyText text={'Укажите имейл и мы пришлем вам пароль'} />
-				<label className={cls.label} htmlFor='email'>
-					{t('email')}
-				</label>
-				<Input
-					autoFocus
-					type='text'
-					id='email'
-					value={email}
-					onChangeString={setEmail}
-				/>
+				<VStack className={cls.content} align='left' gap='gap_4'>
+
+					<Heading size='s' title={'Укажите вашу почту и мы пришлем вам пароль'} as={'h1'} />
+					<label className={cls.label} htmlFor='email'>
+						{t('Email')}:
+					</label>
+					<Input
+						autoFocus
+						// label={t('email')}
+						type='text'
+						id='email'
+						value={email}
+						onChangeString={onChangeEmail}
+					/>
+				</VStack>
 				<ModalButtons
 					onClose={onCloseModal}
-					textCloseButton='назад'
-					textSubmitButton='отправить письмо'
+					onSubmit={onSendPasswordClick}
+					textCloseButton='Назад'
+					textSubmitButton='Восстановить пароль'
 				/>
 			</div>
 
