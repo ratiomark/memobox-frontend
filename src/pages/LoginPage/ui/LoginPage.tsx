5b64c8098b38719d25292f0d30f765f661f35270
ratiomark <yanagae@gmail.com>
2024-04-21 04:54:45 +0300

    ref: remove unused code & prettify

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -2,6 +2,7 @@ import { LoginScreen, loginReducer } from '@/features/AuthByUsername';
 import { TEST_PAGES_IDS } from '@/shared/const/testConsts';
 import { ReducersList, useAsyncReducer } from '@/shared/lib/helpers/hooks/useAsyncReducer';
 import { Page } from '@/widgets/Page';
+import { useEffect } from 'react';
 
 const initialReducers: ReducersList = {
 	loginForm: loginReducer,
@@ -11,8 +12,9 @@ export const LoginPage = () => {
 	useAsyncReducer({
 		reducers: initialReducers,
 		removeAfterUnmount: false,
+		
 	})
-
+	
 	return (
 		<Page data-testid={TEST_PAGES_IDS.loginPage}>
 			<LoginScreen />
