95057f5b07302922e28f449b59fbbf265ab68d2e
ratiomark <yanagae@gmail.com>
2024-04-26 14:22:40 +0300

    fix: add file extension

--- src/shared/const/country-timezones/country-loader.ts
+++ src/shared/const/country-timezones/country-loader.ts
@@ -1,4 +1,4 @@
 export const loadCountries = async (locale: 'en' | 'ru'): Promise<string[]> => {
-	const module = await import(`./countries-${locale}`);
+	const module = await import(`./countries-${locale}.ts`);
 	return module.countries;
 };
