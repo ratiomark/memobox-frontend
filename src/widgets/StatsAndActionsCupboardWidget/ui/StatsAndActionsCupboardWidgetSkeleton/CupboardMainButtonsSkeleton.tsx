daf376d4ba2f76f1158a14eb0386911ffc854009
ratiomark <yanagae@gmail.com>
2024-04-26 14:58:09 +0300

    ref: update skeleton

--- src/widgets/StatsAndActionsCupboardWidget/ui/StatsAndActionsCupboardWidgetSkeleton/CupboardMainButtonsSkeleton.tsx
+++ src/widgets/StatsAndActionsCupboardWidget/ui/StatsAndActionsCupboardWidgetSkeleton/CupboardMainButtonsSkeleton.tsx
@@ -1,14 +1,27 @@
 import cls from '../StatsAndActionsCupboardWidget.module.scss';
-import { DefaultButtonSkeleton } from '@/shared/ui/Skeleton';
-import { InfoButtonSkeleton } from '@/shared/ui/Skeleton/InfoButtonSkeleton/InfoButtonSkeleton';
+import { DefaultButtonSkeleton, Skeleton } from '@/shared/ui/Skeleton';
 
 export const CupboardMainButtonsSkeleton = () => {
-
 	return (
 		<div className={cls.ShelfButtons}>
-			<DefaultButtonSkeleton width={70} />
-			<DefaultButtonSkeleton width={166} />
-			<InfoButtonSkeleton />
+			<DefaultButtonSkeleton width={90} />
+			<DefaultButtonSkeleton width={Number(localStorage.getItem('maxButtonWidth') ?? 160)} className={cls.trainButton} />
+			<Skeleton width={24} height={24} borderRadius='999px' className={cls.arrow} />
+			{/* <InfoButtonSkeleton /> */}
 		</div>
 	)
 }
+// import cls from '../StatsAndActionsCupboardWidget.module.scss';
+// import { DefaultButtonSkeleton } from '@/shared/ui/Skeleton';
+// import { InfoButtonSkeleton } from '@/shared/ui/Skeleton/InfoButtonSkeleton/InfoButtonSkeleton';
+
+// export const CupboardMainButtonsSkeleton = () => {
+
+// 	return (
+// 		<div className={cls.ShelfButtons}>
+// 			<DefaultButtonSkeleton width={70} />
+// 			<DefaultButtonSkeleton width={166} />
+// 			<InfoButtonSkeleton />
+// 		</div>
+// 	)
+// }
