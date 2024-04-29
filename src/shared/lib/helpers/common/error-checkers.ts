a0d2eb22e0625e8525273299caf1a4cf893ec7a4
ratiomark <yanagae@gmail.com>
2024-04-29 20:09:07 +0300

    add: error chechers

---
+++ src/shared/lib/helpers/common/error-checkers.ts
@@ -0,0 +1,22 @@
+interface CustomError {
+	status?: number;
+	error?: string;
+}
+interface CustomErrorFromServer {
+	status?: number;
+	data: {
+		message: string;
+	}
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isErrorWithStatusAndError(error: any): error is CustomError {
+	return 'status' in error && 'error' in error;
+}
+
+export function isErrorWithStatusAndData(error: any): error is CustomErrorFromServer {
+	return 'status' in error && 'data' in error;
+}
+export function isErrorWithMessage(error: any): error is CustomErrorFromServer {
+	return 'status' in error && 'data' in error;
+}
